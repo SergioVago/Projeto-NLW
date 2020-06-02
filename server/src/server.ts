2813489160f098e8f3434e5d69544858ebbd7d5e
Sergio Vago <sergio.vago@hotmail.com>
2020-06-01 21:24:50 -0300

    Aula 1
    Backend:
    - Adcionado o /node_modules ao gitignore
    - Executado o npm init -y
    - Criado o server com a primeira rota (users)
    - Criado o arquivo de configuração do TS
    
    Frontend:
    - Executado o npx create-react-app --template=typescript (para criar o projeto em react utilizando TS)
    - Adicionado a cor roxa ao h1 no css
    - Adcionado o h1 à página inicial

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => {
+    console.log('Listagem de usuários')
+
+    // JSON
+
+    res.json([
+        'Diego',
+        'Cleiton',
+        'Robson',
+        'Daniel'
+    ])
+})
+
+app.listen(3333)
